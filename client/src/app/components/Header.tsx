fe8c658c22a15e891cf9463235e7b020c56f15b3
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-21 23:26:38 +0300

    add: create Header component with title and styling

---
+++ client/src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600">
+      <div className="flex justify-center p-4  w-full ">
+        <div>
+          <h1 className="text-xl text-white font-semibold ">
+            The 9th president of the student Association of JUST
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
