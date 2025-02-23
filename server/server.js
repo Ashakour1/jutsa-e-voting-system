19fc2e525d0993ab4f3925377c6343240834c4e7
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-23 21:12:53 +0300

    add: implement initial server setup with Express and routing

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import UserRoutes from "./routes/user-routes.js";
+import VoteRoutes from "./routes/vote-routes.js";
+import CandidateRoutes from "./routes/candidate-routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/users", UserRoutes);
+
+app.use("/api/votes", VoteRoutes);
+
+app.use("/api/candidates", CandidateRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
