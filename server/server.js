25bf19a571cd39053ba1e814facef9b9c0c91a57
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-04 23:18:36 +0300

    add: enable CORS support in the server for cross-origin requests

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import UserRoutes from "./routes/user-routes.js";
 import VoteRoutes from "./routes/vote-routes.js";
 import CandidateRoutes from "./routes/candidate-routes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
